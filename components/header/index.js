74371622b3743ad9350d72d13330d27e4a261614
Pureum309 <psun27@my.bcit.ca>
2023-01-18 16:26:07 -0800

    first init

---
+++ components/header/index.js
@@ -0,0 +1,28 @@
+import styled from "styled-components"
+
+const Cont = styled.div`
+    width: 100%;
+    height: 50%;
+    background-color: skyblue;
+
+    display: flex;
+    align-items: center;
+`
+const Logo = styled.img`
+    width: 15%;
+    margin: 1em;
+    border-radius: 1em;
+`
+const Txt = styled.h1`
+`
+
+export default function HeaderComp() {
+    return (
+        <>
+            <Cont>
+                <Logo src="./forecast.png" />
+                <Txt>Travel Weather App</Txt>
+            </Cont>
+        </>
+    )
+}
